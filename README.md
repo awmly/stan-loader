5bd21aab672217d46f37c719ac92f5a989271c13
Andrew Womersley <a@aw1.me>
2014-09-09 15:39:11 +0100

    v1 release

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 stan-loader
 ===========
 
-Simple JS Asset loader
+Really simple JS script loader that will load an array of libraries and then execute a callack function.
+
+    <script src='stan-loader.min.js'></script>
+    <script>
+        $STAN_Load([
+            '//code.jquery.com/jquery-1.10.1.min.js',
+            '//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js'
+        ], function(){
+            alert("Loaded");
+        });
+    </script>
--- stan-loader.js
+++ stan-loader.js
@@ -1,72 +1,85 @@
-(function(win,method){
+/*!
+ * STAN Loader 1.0.0
+ * Copyright 2014 Andrew Womersley
+ */
+(function(win, doc) {
 
-  var methodVar = method || '$STAN_Load',
-      loadCount = 0,
-      libs,
-      callback;
+  // Decale variables
+  var methodVar = win.$STAN_Loader || '$STAN_Load',
+    loadCount = 0,
+    libs,
+    callback;
 
+  /*
+   * Set libs and callback from the public method
+   */
+  function init(_libs, _callback) {
 
-  function init(l,c){
+    libs = _libs;
+    callback = _callback;
 
-    libs      = l;
-    callback  = c;
-
-    if(document.addEventListener)
-      document.addEventListener('DOMContentLoaded', addScript );
+    // add window load listeners (W3C : IE8/9)
+    if (doc.addEventListener)
+      doc.addEventListener('DOMContentLoaded', addScript);
     else
-      window.attachEvent('onload', addScript );
+      win.attachEvent('onload', addScript);
 
   }
 
-  function loaded(){
+  /*
+   * Create and add the script to the head
+   */
+  function addScript() {
 
-    loadCount++;
+    var script = doc.createElement('script');
+    script.type = 'text/javascript';
+    script.src = libs[loadCount];
+    script.async = true;
 
-    if(libs[loadCount]){
+    // Set onload events for newly added script
+    if ('onload' in script) { // W3C
 
-      addScript();
+      script.onload = loaded;
 
-    }else{
+    } else { // IE8/9
 
-      callback();
+      script.onreadystatechange = function() {
 
-    }
-
-  }
-
-  function addScript(){
-
-    var   script = document.createElement('script');
-          script.type='text/javascript';
-          script.src=libs[loadCount];
-
-    if('onload' in script){ // IE10+
-
-      script.onload=function(){
-
-        loaded();
-
-      };
-
-    }else{ // IE8/9
-
-      script.onreadystatechange = function () {
-
-        if (this.readyState == 'complete' || this.readyState=='loaded') {
-
+        if (this.readyState == 'complete' || this.readyState == 'loaded')
           loaded();
 
-        }
-
       };
 
     }
 
-    document.getElementsByTagName("head")[0].appendChild(script);
+    /*
+     * Set libs and callback from the public method
+     */
+    function loaded() {
+
+      // Incriment loadCount
+      loadCount++;
+
+      // Check for libs to load - else perform callback
+      if (libs[loadCount]) {
+
+        addScript();
+
+      } else {
+
+        // Check callback is a function
+        if( typeof(callback)=='function' ) callback();
+
+      }
+
+    }
+
+    // Add the html to the head
+    doc.getElementsByTagName("head")[0].appendChild(script);
 
   }
 
-  // declare public methods
+  // declare public method
   win[methodVar] = init;
 
-})(window);
+})(window, document);
