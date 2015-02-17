9e4b2a75e9a6973f5b76b603b01c75423310677d
Andrew Womersley <a@wmly.uk>
2015-02-17 12:49:28 +0000

    Update browsers

--- tests/karma-browsers.js
+++ tests/karma-browsers.js
@@ -1,22 +1,40 @@
 module.exports = {
 
-	'SL_Chrome': {
+	'SL_Chrome_Win7': {
 		base: 'SauceLabs',
 		browserName: 'chrome',
 		platform: 'Windows 7'
 	},
 
-	'SL_IE': {
+	'SL_Chrome_Win8': {
 		base: 'SauceLabs',
-		browserName: 'internet explorer',
-		platform: 'Windows 7',
-		version: '8'
+		browserName: 'chrome',
+		platform: 'Windows 8.1'
 	},
 
-	'SL_FF': {
+	'SL_Chrome_OSX': {
+		base: 'SauceLabs',
+		browserName: 'chrome',
+		platform: 'OS X 10.10'
+	},
+
+	'SL_FF_Win7': {
 		base: 'SauceLabs',
 		browserName: 'firefox',
 		platform: 'Windows 7'
+	},
+
+	'SL_FF_OSX': {
+		base: 'SauceLabs',
+		browserName: 'firefox',
+		platform: 'OS X 10.10'
+	},
+
+	'SL_IE_9': {
+		base: 'SauceLabs',
+		browserName: 'internet explorer',
+		platform: 'Windows 7',
+		version: '9'
 	},
 
 	'SL_Safari': {
