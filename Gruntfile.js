c950fa341002a92f551fb3587dc310a2770ca6f2
Andrew Womersley <a@wmly.uk>
2015-02-17 21:26:12 +0000

    Update grunt server info

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,52 +1,54 @@
 module.exports = function(grunt) {
 
-    var config = {
+	var config = {
 
-        pkg: grunt.file.readJSON('package.json'),
+		pkg: grunt.file.readJSON('package.json'),
 
-        banner: '/*!\n' + ' * STAN Loader <%= pkg.version %>\n' + ' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' + ' */\n',
+		banner: '/*!\n' + ' * STAN Loader <%= pkg.version %>\n' + ' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' + ' */\n',
 
-        watch: {
-            html: {
-                files: ['index.html']
-            },
-            js: {
-                files: ['stan-loader.js'],
-                tasks: ['uglify']
-            }
-        },
+		watch: {
+			html: {
+				files: ['index.html']
+			},
+			js: {
+				files: ['stan-loader.js'],
+				tasks: ['uglify']
+			}
+		},
 
-        connect: {
-            server: {
-                options: {
-                    port: 8080,
-                    open: true,
-                }
-            }
-        },
+		connect: {
+			server: {
+				options: {
+					base: ["tests", "./"],
+					hostname: "0.0.0.0",
+					port: 8080,
+					open: true
+				}
+			}
+		},
 
-        uglify: {
-            dist: {
-                options:{
-                  banner : '<%= banner %>',
-                },
-                files: {
-                    'stan-loader.min.js': ['stan-loader.js']
-                }
-            }
-        }
+		uglify: {
+			dist: {
+				options: {
+					banner: '<%= banner %>',
+				},
+				files: {
+					'stan-loader.min.js': ['stan-loader.js']
+				}
+			}
+		}
 
-    };
+	};
 
-    // Init grunt config
-    grunt.initConfig(config);
+	// Init grunt config
+	grunt.initConfig(config);
 
-    // Load node modules
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+	// Load node modules
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
-    // Register tasks
-    grunt.registerTask('default', ['uglify','connect','watch']);
+	// Register tasks
+	grunt.registerTask('default', ['uglify', 'connect', 'watch']);
 
 };
