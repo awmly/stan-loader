7a9044f7283c5036ae25e05ec009a13368858680
Andrew Womersley <a@wmly.uk>
2015-02-17 13:33:55 +0000

    Add browsers

--- tests/karma-browsers.js
+++ tests/karma-browsers.js
@@ -4,44 +4,51 @@ module.exports = {
 		base: 'SauceLabs',
 		browserName: 'chrome',
 		platform: 'Windows 7'
+	},
+
+	'SL_Chrome_OSX': {
+		base: 'SauceLabs',
+		browserName: 'chrome',
+		platform: 'OS X 10.10'
+	},
+
+	'SL_FF_Win7': {
+		base: 'SauceLabs',
+		browserName: 'firefox',
+		platform: 'Windows 7'
+	},
+
+	'SL_FF_OSX': {
+		base: 'SauceLabs',
+		browserName: 'firefox',
+		platform: 'OS X 10.10'
+	},
+
+	'SL_IE_9': {
+		base: 'SauceLabs',
+		browserName: 'internet explorer',
+		platform: 'Windows 7',
+		version: '9'
+	},
+
+	'SL_IE_10': {
+		base: 'SauceLabs',
+		browserName: 'internet explorer',
+		platform: 'Windows 7',
+		version: '10'
+	},
+
+	'SL_IE_11': {
+		base: 'SauceLabs',
+		browserName: 'internet explorer',
+		platform: 'Windows 7',
+		version: '11'
+	},
+
+	'SL_Safari': {
+		base: 'SauceLabs',
+		browserName: 'safari',
+		platform: 'OS X 10.10'
 	}
-	/*,
-
-		'SL_Chrome_Win8': {
-			base: 'SauceLabs',
-			browserName: 'chrome',
-			platform: 'Windows 8.1'
-		},
-
-		'SL_Chrome_OSX': {
-			base: 'SauceLabs',
-			browserName: 'chrome',
-			platform: 'OS X 10.10'
-		},
-
-		'SL_FF_Win7': {
-			base: 'SauceLabs',
-			browserName: 'firefox',
-			platform: 'Windows 7'
-		},
-
-		'SL_FF_OSX': {
-			base: 'SauceLabs',
-			browserName: 'firefox',
-			platform: 'OS X 10.10'
-		},
-
-		'SL_IE_9': {
-			base: 'SauceLabs',
-			browserName: 'internet explorer',
-			platform: 'Windows 7',
-			version: '9'
-		},
-
-		'SL_Safari': {
-			base: 'SauceLabs',
-			browserName: 'safari',
-			platform: 'OS X 10.10'
-		}*/
 
 }
